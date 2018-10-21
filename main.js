4d00c3aef923279ef5214330e89148eadfae7331
ViviGong <gongweiuibe@gmail.com>
2018-10-20 23:57:10 -0400

    commit

--- main.js
+++ main.js
@@ -1,24 +1,80 @@
-var picts = ["taro.png", "bun2.jpg", "bunny.jpg"]
-var names = ["Steve", "Finn", "Jake", "Taro", "Luna"]
-var age = [2, 3, 89, 680, 1, 0]
-
-
-class Animal{
-
-    constructor(img, name, age)
-    {
-        this.picts = img
-        this.name = name
-        this.age = age
-    }
+/*** Object Constructors ***/
+function Cat(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image = "cat.jpg";
+  this.type = "Cat";
 }
 
+function Dog(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image = "dog.jpg"
+  this.type = "Dog";
+}
 
-$(document).ready(()=>{
-    console.log("ready")
+function Bird(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image = "bird.jpg"
+  this.type = "Bird";
+}
 
-    var animal = new Animal(picts[1], names[2], age[4])
-    console.log(animal)
-    $("#animal-img").attr("src", animal.pic);
+/*** Global Variables ***/
+var animals = [new Cat(), new Dog(), new Bird()];
+var names = ["Toothless", "Marshmallow", "Momo", "Coco", "Ollie", "Oscar", "Bella", "Ruby", "Apples"];
 
-})
+/*** Functions ***/
+// get a random index for an array from 0 to maxIndex (not inclusive)
+function getRandomIndex(maxIndex) {
+  return Math.floor(Math.random() * maxIndex);
+}
+
+// generates either a Cat, Dog, or Bird with a random name and random age
+function generateRandomAnimal() {
+  var randomIdx = getRandomIndex(animals.length);
+  var randomAnimal = animals[randomIdx];
+
+  if (randomAnimal instanceof Cat)
+  {
+    return new Cat(generateRandomName(), generateRandomAge());
+  }
+  else if (randomAnimal instanceof Dog)
+  {
+    return new Dog(generateRandomName(), generateRandomAge());
+  }
+  else if (randomAnimal instanceof Bird)
+  {
+    return new Bird(generateRandomName(), generateRandomAge());
+  }
+}
+
+// generates a random name from list of names
+function generateRandomName() {
+  var randomIdx = getRandomIndex(names.length);
+  return names[randomIdx];
+}
+
+// generates a random age from 0 to 5
+function generateRandomAge() {
+  var randomIdx = getRandomIndex(5);
+  return randomIdx;
+}
+
+/*** Document Load ****/
+$(document).ready(function() {
+
+  // generate a random animal when the document opens
+  var animal = generateRandomAnimal();
+  // update the page based on the animal properties
+  $("#animal-properties").text(animal.name + "  " + animal.age + "years old");
+  $("#animal-img").attr("src", animal.image);
+
+});
+
+
+
+
+
+
+
